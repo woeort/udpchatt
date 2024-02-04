d215ca8c55a110782bf6ee9b3be4faad8ced1965
woeort <pyntox.channel@yandex.ru>
2024-02-04 18:15:00 +0400

    git add

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+Console.Write("Введите свое имя: ");
+string? username = Console.ReadLine();
+Console.Write("Введите порт для приема сообщений: ");
+if (!int.TryParse(Console.ReadLine(), out var localPort)) return;
+Console.Write("Введите порт для отправки сообщений: ");
+if (!int.TryParse(Console.ReadLine(), out var remotePort)) return;
+Console.WriteLine();
+
+Task.Run(ReceiveMessageAsync);
+await SendMessageAsync();
+
+async Task SendMessageAsync()
+{
+    using Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
+    while (true)
+    {
+        var message = Console.ReadLine(); 
+        if (string.IsNullOrWhiteSpace(message)) break;
+        message = $"{username}: {message}";
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        await sender.SendToAsync(data, new IPEndPoint(localAddress, remotePort));
+    }
+}
+async Task ReceiveMessageAsync()
+{
+    byte[] data = new byte[65535];
+    using Socket receiver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    receiver.Bind(new IPEndPoint(localAddress, localPort));
+    while (true)
+    {
+        var result = await receiver.ReceiveFromAsync(data, new IPEndPoint(IPAddress.Any, 0));
+        var message = Encoding.UTF8.GetString(data, 0, result.ReceivedBytes);
+        Console.WriteLine(message);
+    }
+}
